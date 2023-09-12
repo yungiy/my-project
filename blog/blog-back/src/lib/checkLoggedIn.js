e65b8ae88bec6a366c7dacd21d72a47ef3a2c293
yungiy <devyungiy@gmail.com>
2023-09-12 23:18:11 +0900

    [삭제] 프로젝트 삭제

--- blog/blog-back/src/lib/checkLoggedIn.js
+++
@@ -1,9 +0,0 @@
-const checkLoggedIn = (ctx, next) => {
-	if (!ctx.state.user) {
-		ctx.status = 401;
-		return;
-	}
-	return next();
-};
-
-export default checkLoggedIn;
--- blog/blog-front/src/pages/PostListPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function PostListPage() {
-	return <div>PostListPage</div>;
-}

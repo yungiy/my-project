bdf55b82fe586a7a3794d8fe0f8bd27c4ed3299b
yungiy <devyungiy@gmail.com>
2025-01-30 15:26:29 +0900

    .
    .

--- tw-clone/src/app/(beforeLogin)/i/flow/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-import SignupModal from "../../../_component/SignupModal";
-
-export default function Signup() {
-  return (
-    <SignupModal />
-  )
-}
--- tw-clone/src/app/(beforeLogin)/page.tsx
+++
@@ -1,7 +0,0 @@
-import Main from "./_component/Main";
-
-export default function Home() {
-  return (
-    <Main />
-  )
-}
